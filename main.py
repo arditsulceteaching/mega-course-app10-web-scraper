37205353425f773f1b756fbacdd2e5b6a735c2d1
Ardit <arditsulce@gmail.com>
2022-10-18 10:38:47 +0200

    Day 38: Send email when new event is found

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 import selectorlib
+import smtplib, ssl
+import os
+
 
 
 URL = "http://programmer100.pythonanywhere.com/tours/"
@@ -19,7 +22,19 @@ def extract(source):
     return value
 
 
-def send_email():
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "app8flask@gmail.com"
+    password = "ooruzcufqubxjfsa"
+
+    receiver = "app8flask@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
     print("Email was sent!")
 
 
@@ -40,4 +55,4 @@ if __name__ == "__main__":
     if extracted != "No upcoming tours":
         if extracted not in content:
             store(extracted)
-            send_email()
+            send_email(message="Hey, new event was found!")
